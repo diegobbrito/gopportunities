c7cfba608e151cac32ebf212e35108c3f3d23cdc
Diego Brito <dibbrito@gmail.com>
2025-06-14 17:02:39 -0300

    feat: add config and logger packages with initialization logic

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/diegobbrito/gopportunities/router"
+import (
+	"github.com/diegobbrito/gopportunities/config"
+	"github.com/diegobbrito/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main") // Initialize the logger
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
 	router.Initialize() // Initialize the router
 }
