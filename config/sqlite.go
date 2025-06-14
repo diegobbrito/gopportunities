fc631faedbbd967ee3b213619717e43f2e0e52d0
Diego Brito <dibbrito@gmail.com>
2025-06-14 17:22:21 -0300

    feat: add .gitignore and implement SQLite initialization logic with error handling

---
+++ config/sqlite.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"os"
+
+	"github.com/diegobbrito/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Infof("Database file does not exist, creating: %s", dbPath)
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			logger.Errorf("Error creating database directory: %v", err)
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			logger.Errorf("Error creating database file: %v", err)
+			return nil, err
+		}
+		defer file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("Error opening SQLite database: %v", err)
+		return nil, err
+	}
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("Error migrating schemas: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
