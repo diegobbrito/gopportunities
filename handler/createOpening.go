77e7b69449c1b2fe603906d137b8b35a1a656bfb
Diego Brito <dibbrito@gmail.com>
2025-06-15 17:46:44 -0300

    feat: implement CreateOpeningHandler with validation and database integration; add OpeningResponse struct

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,13 +3,35 @@ package handler
 import (
 	"net/http"
 
+	"github.com/diegobbrito/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func CreateOpeningHandler() func(ctx *gin.Context) {
-	return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "POST opening",
-		})
+func CreateOpeningHandler(ctx *gin.Context) {
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
 	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	sendSuccess(ctx, "createOpening", opening)
 }
