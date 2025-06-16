c997b5687c2efc8a6eb462ae5253540fdb66357f
Diego Brito <dibbrito@gmail.com>
2025-06-16 20:16:55 -0300

    feat: implement DeleteOpeningHandler with error handling and response for opening deletion

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/diegobbrito/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("Opening with id %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("Failed to delete opening with id %s", id))
+		return
+	}
+	sendSuccess(ctx, "delete-opening", opening)
 }
